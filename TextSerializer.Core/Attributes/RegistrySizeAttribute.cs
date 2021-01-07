275d98079a3a0a5e8189a2abdb3be21da8f6dc2e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-07 13:01:25 -0300

    Add RegistrySizeAttribute

---
+++ TextSerializer.Core/Attributes/RegistrySizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextSerializer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class RegistrySizeAttribute : Attribute
+    {
+        public int Length { get; }
+
+        public RegistrySizeAttribute(int length)
+        {
+            Length = length;
+        }
+    }
+}
