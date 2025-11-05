26af1f93fe53bbc24f2db101b123b21bd24d2ebf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-11-05 19:21:02 -0300

    Update program to top-level

--- TextSerializer.Test/Program.cs
+++ TextSerializer.Test/Program.cs
@@ -1,50 +1,42 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using TextSerializer;
+using TextSerializer.Test;
 
-namespace TextSerializer.Test
+Console.WriteLine("Hello World!");
+
+var example = new Example()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    DocumentType = 0,
+    Operation = 1,
+    OperationName = "OPNAME",
+    ServiceCode = 2,
+    ServiceName = "SERVNAME",
+    ActionCode = "3",
+    FunctionCode = "7",
+    CheckDigit = "4",
+    CompanyName = "Example LTD",
+    IssuerCode = "123",
+    IssuerName = "Issuer LTD",
+    CreationDate = DateTime.Now,
+    SequencialCode = 1,
+};
 
-            var example = new Example()
-            {
-                DocumentType = 0,
-                Operation = 1,
-                OperationName = "OPNAME",
-                ServiceCode = 2,
-                ServiceName = "SERVNAME",
-                ActionCode = "3",
-                FunctionCode = "7",
-                CheckDigit = "4",
-                CompanyName = "Example LTD",
-                IssuerCode = "123",
-                IssuerName = "Issuer LTD",
-                CreationDate = DateTime.Now,
-                SequencialCode = 1,
-            };
+var opt = new SerializationOptions()
+{
+    DateTimeFormat = "ddMMyy"
+};
+LineSerializer line = new LineSerializer(opt);
 
-            var opt = new SerializationOptions()
-            {
-                 DateTimeFormat = "ddMMyy"
-            };
-            LineSerializer line = new LineSerializer(opt);
+using var ms = new MemoryStream();
+using var sw = new StreamWriter(ms);
+line.Serialize(example, sw);
 
-            using var ms = new MemoryStream();
-            using var sw = new StreamWriter(ms);
-            line.Serialize(example, sw);
+var text = Encoding.UTF8.GetString(ms.ToArray());
 
-            var text = Encoding.UTF8.GetString(ms.ToArray());
-            
-            Console.WriteLine(text);
+Console.WriteLine(text);
 
 
-            // Convert back
-            var example2 = line.Deserialize<Example>(text.Replace("\r\n", ""), out _);
-
-        }
-    }
-}
+// Convert back
+var example2 = line.Deserialize<Example>(text.Replace("\r\n", ""), out _);
