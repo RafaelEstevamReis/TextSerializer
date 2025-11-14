2ac5f9d0ef43b0eaec8c411ae229c08e9f8f65f2
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-11-14 10:37:04 -0300

    Update IFormatter.cs

--- TextSerializer.Core/Formatters/IFormatter.cs
+++ TextSerializer.Core/Formatters/IFormatter.cs
@@ -4,6 +4,5 @@ public interface IFormatter
 {
     SerializationOptions Options { get; }
     string Serialize(SerializableValue Value);
-    //bool Deserialize(string Line, int Offset, SerializableValue Value);
     bool Deserialize(string Block, SerializableValue Value);
 }
